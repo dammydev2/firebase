0355b52404475e19ed476ffc076d67fb9f0fd562
dammydev2 <yd4u2c@yahoo.com>
2020-04-23 12:13:46 +0100

    navigation

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import Home from './src/screens/Home';
+import AddItem from './src/screens/AddItem';
+import List from './src/screens/List';
+import { render } from 'react-dom';
+import SafeAreaView from 'react-native-safe-area-view';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const AppNavigator = createStackNavigator(
+  {
+    Home,
+    AddItem,
+    List
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default class App extends Component {
+
+  render() {
+
+    return (
+      <AppContainer />
+    )
+
+  }
+
 }
 
 const styles = StyleSheet.create({
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { component, Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default class Home extends Component {
 
@@ -7,8 +7,27 @@ export default class Home extends Component {
         return (
             <View>
                 <Text>Home Page</Text>
+                <View style={styles.button}>
+                    <Button title='add an item'
+                        onPress={() => this.props.navigation.navigate('AddItem')}
+                    />
+                </View>
+                <View  style={styles.button}>
+                    <Button
+                        title="List of Items"
+                        color="green"
+                        onPress={() => this.props.navigation.navigate('List')}
+                    />
+                </View>
             </View>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    button: {
+        width: 200,
+        padding: 20
+    }
+})
