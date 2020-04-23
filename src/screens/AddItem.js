c2a9ff94dc3d622570a6cbcd7c965f2d1a08b85f
dammydev2 <yd4u2c@yahoo.com>
2020-04-23 03:51:42 +0100

    stack navigation

---
+++ src/screens/AddItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class AddItem extends Component {
+
+    render() {
+
+        return(
+            <View>
+                <Text>This is Add item page</Text>
+            </View>
+        )
+
+    }
+
+}
