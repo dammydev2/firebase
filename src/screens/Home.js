40f62dac27c27e94f558d9c0b5a8ba32d88521f5
dammydev2 <yd4u2c@yahoo.com>
2020-04-23 03:42:56 +0100

    home page

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React, { component, Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Home extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Home Page</Text>
+            </View>
+        )
+    }
+
+}
